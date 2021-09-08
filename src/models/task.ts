9158ee87e037b9b4ea39b685de72b2a061566831
iptkrisna <krisnamath9@gmail.com>
2021-09-09 01:20:13 +0800

    fix task API issue

--- src/models/task.ts
+++ src/models/task.ts
@@ -1,12 +1,13 @@
 import { BaseTask, Task, TaskDetail } from "../types/task";
 import db from "../services/db";
-import { OkPacket, RowDataPacket } from "mysql2";
+import { OkPacket, QueryError, RowDataPacket } from 'mysql2';
+import { PayloadUser, User } from '../types/user';
 
 const create = (task: BaseTask): Promise<number | null> => new Promise((resolve, reject) => {
-  const queryString = "INSERT INTO Task (description,image, dueDate, isComplete) VALUES (?,?,?,?)";
+  const queryString = "INSERT INTO Task (user, description,image, dueDate, isComplete) VALUES (?,?,?,?,?)";
   db.query(
     queryString,
-    [task.description, task.image, task.dueDate, task.isComplete],
+    [task.user.email, task.description, task.image, task.dueDate, task.isComplete],
     (err, result) => {
       if (err) return reject(err);
 
@@ -16,40 +17,80 @@ const create = (task: BaseTask): Promise<number | null> => new Promise((resolve,
   );
 });
 
-const update = (task: BaseTask): Promise<number | null> => new Promise((resolve, reject) => {
-  const queryString = "UPDATE Task SET description=?,image=?, dueDate=?, isComplete = ?  WHERE user = ?";
+const findOne = (id: number) : Promise<Task | null> => new Promise((resolve, reject) => {
+  const queryString = `SELECT * from Task WHERE id=?`;
 
-  db.query(
-    queryString,
-    [task.description, task.image, task.image, task.dueDate, task.isComplete],
-    (err, result) => {
-      if (err) return reject(err);
-      const insertId = (result as OkPacket).insertId;
-      resolve(insertId);
-    }
-  );
-});
-
-const del = (task: BaseTask): Promise<number | null> => new Promise((resolve, reject) => {
-  const queryString = "DELETE FROM Task WHERE user = ?";
-
-  db.query(queryString, (err, result) => {
+  console.log(id);
+  db.query(queryString, id, (err, result) => {
+    // console.log("findOneV2", err, result)
     if (err) return reject(err);
 
+    console.log(result);
+    if (!(result as RowDataPacket).length) return resolve(null);
+
+    const row = (result as RowDataPacket)[0];
+    const task: Task = {
+      id: row.id,
+      user: {email: row.user},
+      description: row.description,
+      dueDate: row.dueDate,
+      image: row.image,
+      isComplete: row.isComplete,
+    };
+    resolve(task);
   });
 });
 
-const listAll = (task: TaskDetail): Promise<number | null> => new Promise((resolve, reject) => {
-  const queryString = "SELECT * FROM Task WHERE user = ?";
-  db.query(queryString, (err, result) => {
-    if (err) return reject(err);
-    const row = (result as RowDataPacket)[0];
+const update = (task: Task): Promise<number | null> => new Promise((resolve, reject) => {
+  const queryString = "UPDATE Task SET description=?,image=?, dueDate=?, isComplete = ?  WHERE id=?";
 
+  db.query(
+    queryString,
+    [task.description, task.image, task.dueDate, task.isComplete, task.id],
+    (err, result) => {
+      if (err) return reject(err);
+      const insertId = (result as OkPacket).insertId;
+      resolve(insertId);
+    }
+  );
+});
+
+const del = (id: number): Promise<QueryError | null> => new Promise((resolve, reject) => {
+  const queryString = "DELETE FROM Task WHERE id=?";
+
+  db.query(queryString, id, (err) => {
+    if (err) return reject(err);
+
+    resolve(null)
+  });
+});
+
+const listAll = (user: PayloadUser): Promise<Array<Task> | null> => new Promise((resolve, reject) => {
+  const queryString = "SELECT * FROM Task WHERE user = ?";
+  db.query(queryString, user.email, (err, result) => {
+    if (err) return reject(err);
+
+    if (!(result as RowDataPacket[]).length) return resolve(null);
+
+    const rows = (result as RowDataPacket[]);
+
+    const tasks: Array<Task> = [];
+    rows.forEach(row => {
+      tasks.push({
+        id: row.id,
+        user: row.user,
+        description: row.description,
+        dueDate: row.dueDate,
+        image: row.image,
+        isComplete: row.isComplete
+      })
+    });
+    resolve(tasks)
   });
 });
 
 const TaskModel = {
-  create, update, del, listAll
+  create, update, del, listAll, findOne
 };
 
 export default TaskModel;
--- src/routes/task.ts
+++ src/routes/task.ts
@@ -1,15 +1,28 @@
-import express from "express";
+import express from 'express';
 const taskRouter = express.Router();
-import { body } from 'express-validator';
-import { getTodos, addTodo, updateTodo, deleteTodo } from "../api/task"
+import { body, query } from 'express-validator';
+import taskAPI from "../api/task"
+import authMiddleware from '../middleware/auth';
+import uploadFile from '../middleware/upload'
 
+taskRouter.get("", authMiddleware, taskAPI.getTodos);
 
-taskRouter.get("/todo", getTodos)
+taskRouter.post("", [
+  authMiddleware,
+  uploadFile.single('file'),
+  body('description', 'Please provide a description').notEmpty(),
+  body('dueDate', 'Please provide a description').isDate({format: 'YYYY-MM-DD'})
+], taskAPI.addTodo);
 
-taskRouter.post("/add-todo", addTodo)
+taskRouter.put("", [
+  authMiddleware,
+  uploadFile.single('file'),
+  query('id', 'Please provide a valid id').isNumeric()
+], taskAPI.updateTodo);
 
-taskRouter.put("/edit-todo/:id", updateTodo)
-
-taskRouter.delete("/delete-todo/:id", deleteTodo)
+taskRouter.delete("", [
+  authMiddleware,
+  query('id', 'Please provide a valid id').isNumeric()
+], taskAPI.deleteTodo);
 
 export default taskRouter;
